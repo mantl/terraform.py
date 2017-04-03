7ce23f9375546ddbd9753168d167b250ad727157
Sean Abbott <sean.abbott@gmail.com>
2017-04-03 17:54:39 -0400

    add a test to check if a state file A) is remote and B) is s3

--- tests/remote/test_s3.py
+++ tests/remote/test_s3.py
@@ -1,8 +1,26 @@
+# -*- coding: utf-8 -*-
+import boto3
+import json
 from moto import mock_s3
 
-import boto3  # noqa
+from ati.remote import s3_remote_state as s3rs
 
 @mock_s3
 def test_get_remote_state():
     conn = boto3.resource('s3', region_name='us-east-1')
     conn.create_bucket(Bucket='terraform')
+    # nothing happening yet
+
+def test_is_remote_state():
+    tf = None
+    with open('tests/fixtures/remote_init.json', 'r') as f:
+        tf = json.load(f)
+
+    assert s3rs.is_remote_state(tf)
+
+def test_not_remote_state():
+    tf = None
+    with open('tests/fixtures/local_init.json', 'r') as f:
+        tf = json.load(f)
+
+    assert not s3rs.is_remote_state(tf)
