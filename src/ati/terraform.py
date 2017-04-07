7b4830bbba3f5672f84187cfd14ce9c82ae90e4e
Sean Abbott <sean.abbott@gmail.com>
2017-04-07 14:00:46 -0400

    add ability to include an ssh key via tags.sshPrivateKey. this may be relative to where ansible is called from.

--- src/ati/terraform.py
+++ src/ati/terraform.py
@@ -559,6 +559,8 @@ def aws_host(resource, module_name, **kwargs):
         attrs['ansible_ssh_user'] = raw_attrs['tags.sshUser']
     if 'tags.sshPrivateIp' in raw_attrs:
         attrs['ansible_ssh_host'] = raw_attrs['private_ip']
+    if 'tags.sshPrivateKey' in raw_attrs:
+        attrs['ansible_ssh_private_key_file'] = raw_attrs['tags.sshPrivateKey']
 
     # add to groups by comma separated tag(s)
     if 'tags.groups' in raw_attrs:
