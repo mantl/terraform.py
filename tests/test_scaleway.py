0540927555978893de9750800d96efef6c84daac
Stephen Gagliardo <stephen.gagliardo@gmail.com>
2017-05-04 11:15:32 -0400

    squashed a few more spelling and other bugs

--- tests/test_scaleway.py
+++ tests/test_scaleway.py
@@ -65,6 +65,7 @@ def test_name(scaleway_resource, scaleway_host):
     'volume.0.volume_id': '282baeb3-f87b-45cf-b423-790fd95c5ad1',
     # ansible
     'ansible_ssh_host': '77.77.77.77',
+    'ansible_ssh_port': 22,
     'ansible_ssh_user': 'root',
     # generic
     'private_ipv4': '55.55.55.55',
@@ -80,7 +81,7 @@ def test_attrs(scaleway_resource, scaleway_host, attr, should):
 @pytest.mark.parametrize(
     'group',
     ['scaleway_image=7258ac9b-61e7-4f69-a72d-b424de25fe84', 'scaleway_type=VC1M', 'scaleway_state=running',
-     'scalway_tag=frontend', 'scaleway_tag=dev', 'scaleway_tag=lga'])
+     'scaleway_tag=frontend', 'scaleway_tag=dev', 'scaleway_tag=lga'])
 def test_groups(scaleway_resource, scaleway_host, group):
     _, _, groups = scaleway_host(scaleway_resource, 'module_name')
     assert group in groups
